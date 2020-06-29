a68492b47f16f3940579a7a19966ab9735b8484a
Miguel Cardenas <miguelcarde.3@gmail.com>
2020-06-29 16:43:43 -0500

    product card styles

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import Container from './products.styled';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Container, ProductCard } from './products.styled';
+import { AssetsContainer } from '~/src/components/gallery/gallery.styled';
 import { SectionTitle } from '~/src/commons/title';
 
 const Products = () => (
   <Container>
     <SectionTitle>Nuestros productos</SectionTitle>
+    <AssetsContainer>
+      <Carousel
+        showThumbs={false}
+        showIndicators={false}
+        infiniteLoop={true}
+        swipeable={false}
+      >
+        <ProductCard>
+          <div className="card">
+            <div className="product-image" />
+            <div className="product-description">
+              <div className="product-price">
+                <span>Pan de queso</span>
+                <span>Pan andino relleno de queso paisa</span>
+                <span className="price">9.99$</span>
+              </div>
+              {/* contador */}
+            </div>
+          </div>
+        </ProductCard>
+      </Carousel>
+    </AssetsContainer>
   </Container>
 );
 
--- src/components/products/products.styled.js
+++ src/components/products/products.styled.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
+import colors from '~/src/styles/colors';
+import panCuadrado from '../../assets/images/pan-cuadrado.jpg';
 
-const Container = styled.div`
+export const Container = styled.div`
   width: 100%;
   height: 100%;
   background: linear-gradient(rgba(63, 60, 59, 0.99), rgba(242, 237, 238, 0.9));
@@ -26,4 +28,53 @@ const Container = styled.div`
   }
 `;
 
-export default Container;
+export const ProductCard = styled.section`
+  background: linear-gradient(
+    to left,
+    rgba(242, 238, 238, 0.5),
+    rgba(63, 60, 59, 0.5)
+  );
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > .card {
+    background: ${colors.white};
+    width: 50%;
+    height: 90%;
+    border-radius: 5px;
+    > .product-image {
+      background: url(${panCuadrado});
+      border-radius: 5px;
+      width: 100%;
+      height: 55%;
+    }
+    > .product-description {
+      width: 100%;
+      height: 45%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      > .product-price {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        > span {
+          color: ${colors.letters};
+          font-size: 1.7em;
+          font-family: 'KaushanScript-Regular';
+        }
+        > .price {
+          font-size: 2.2em;
+          font-family: 'Oswald-Regular';
+          margin-top: 1em;
+        }
+      }
+    }
+  }
+`;
