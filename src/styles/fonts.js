01698868c4364fc073a79a2ccd7bf186c812e8b3
Miguel Cardenas <miguelcarde.3@gmail.com>
2020-08-31 17:47:37 -0500

    delete unused font

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,16 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
-import KaushanScript from '~/src/assets/fonts/KaushanScript-Regular.ttf';
 import Oswald from '~/src/assets/fonts/Oswald-Regular.ttf';
 import HeyAugust from '~/src/assets/fonts/Hey-August.ttf';
 
 const GlobalFonts = createGlobalStyle`
-    @font-face {
-        font-family: 'KaushanScript-Regular';
-        src: url(${KaushanScript}) format('truetype');
-        font-weight: normal;
-        font-style: normal;
-    }
+    
     @font-face {
         font-family: 'Oswald-Regular';
         src: url(${Oswald}) format('truetype');
